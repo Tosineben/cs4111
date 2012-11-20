676ef4ddd8d58bfc581d07f055c8d6a6afca1f61
Michal Wolski <michalwols@gmail.com>
2012-11-20 10:07:58 -0500

    messageServlet

--- src/courseworks/MessageServlet.java
+++ src/courseworks/MessageServlet.java
@@ -1,6 +1,5 @@
 package courseworks;
 
-import courseworks.model.Announcement;
 import courseworks.model.Message;
 import courseworks.model.Student;
 import courseworks.sql.CourseworksWriter;
@@ -13,35 +12,29 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "CoursePageServlet")
-public class CoursePageServlet extends HttpServlet {
+@WebServlet(name = "MessageServlet")
+public class MessageServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         PrintWriter pw = response.getWriter();
-        boolean success = false;
 
         Student student = (Student)request.getSession().getAttribute(SessionKeys.logged_in_student);
-        String type = request.getParameter("type");
 
-        if(student != null){
-            if("read".equals(type)){
-                Announcement ancmt = new Announcement();
-                ancmt.anncmnt_id = Integer.parseInt(request.getParameter("announcement_id"));
-                success = ancmt.updateAnncmntRead(student.uni, true);
-                if(success){
-                    pw.print("worked");
-                }
-                else {
-                    response.sendError(500);
-                }
+            Message msg = new Message();
+            msg.author = student;
+            msg.message = request.getParameter("message");
+            int event_id = Integer.parseInt(request.getParameter("event_id"));
+            CourseworksWriter wtr = new CourseworksWriter();
+            int num = wtr.createMessage(event_id, msg);
 
+            if(num > 0 ){
+                pw.print("worked");
+            }
+            else{
+                response.sendError(500);
             }
 
 
-        }
-
-
-        pw.close();
 
     }
 
